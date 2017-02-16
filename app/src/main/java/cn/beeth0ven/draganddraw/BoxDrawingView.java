00cd766441d101268eb7f288dd0f4beb97891d12
beeth0ven <beeth0vendev@163.com>
2017-02-17 00:07:34 +0800

    Initial commit

---
+++ app/src/main/java/cn/beeth0ven/draganddraw/BoxDrawingView.java
@@ -0,0 +1,45 @@
+package cn.beeth0ven.draganddraw;
+
+import android.content.Context;
+import android.graphics.PointF;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by Air on 2017/2/16.
+ */
+
+public class BoxDrawingView extends View {
+
+    public BoxDrawingView(Context context) {
+        this(context, null);
+    }
+
+    public BoxDrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        PointF current = new PointF(event.getX(), event.getY());
+        String action = "";
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                action = "ACTION_DOWN";
+                break;
+            case MotionEvent.ACTION_MOVE:
+                action = "ACTION_MOVE";
+                break;
+            case MotionEvent.ACTION_UP:
+                action = "ACTION_UP";
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                action = "ACTION_CANCEL";
+                break;
+        }
+        Log.i("BoxDrawingView", action + " at x=" + current.x + ", y=" + current.y);
+        return true;
+    }
+}
---
+++ app/src/main/java/cn/beeth0ven/draganddraw/DragAndDrawFragment.java
@@ -0,0 +1,26 @@
+package cn.beeth0ven.draganddraw;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Air on 2017/2/16.
+ */
+
+public class DragAndDrawFragment extends Fragment {
+
+    public static DragAndDrawFragment newInstance() {
+        return new DragAndDrawFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.drag_and_draw_fragment, container, false);
+        return view;
+    }
+}
